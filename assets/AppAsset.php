87fce1ff9bd71fd4c27c809a31dbaae84dc061b1
Alexotieno1717 <alexotieno900@gmail.com>
2024-02-15 11:38:38 +0300

    [feat]: integrated bootstrap design into the project

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,9 +20,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/styles.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
+        'https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700',
+        'https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic',
+        'https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css'
     ];
     public $js = [
+        'js/scripts.js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js',
+        'https://cdn.startbootstrap.com/sb-forms-latest.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
