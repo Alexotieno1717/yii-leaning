fc29e47d6a521286147d5ac24436435fcbf3366e
Alexotieno1717 <alexotieno900@gmail.com>
2024-02-19 12:16:30 +0300

    [feat]: modified the login page

--- views/site/login.php
+++ views/site/login.php
@@ -11,10 +11,10 @@ use yii\bootstrap5\Html;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="container pt-5 mt-5">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p >Please fill out the following fields to login:</p>
 
     <div class="row">
         <div class="col-lg-5">
